18061c0941b4b1adfd12c6e3a1092b363c5e66db
Patrick Winters <19wintersp@gmail.com>
2025-08-06 01:14:49 +0100

    feat(config): new config format
    * unidirectional block settings
    
    * expanded styling options
    
    * geo map separation
    
    * countdown widget
    
    * sticky nodes for block routing
    
    client pending further refactor

--- shared/config/src/lib.rs
+++ shared/config/src/lib.rs
@@ -1,23 +1,27 @@
-use std::fmt::Debug;
-use std::io::{Read, Write};
+mod map;
 
-pub use bincode;
-use bincode::{DefaultOptions, ErrorKind, Options};
+use std::cmp::Ordering;
+use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
+use std::io::{Error as IoError, Read, Write};
+use std::marker::PhantomData;
+
+use bincode::config::Configuration as BincodeConfig;
+use bincode::error::{DecodeError, EncodeError};
+use bincode::{Decode, Encode};
 
 use flate2::read::DeflateDecoder;
 use flate2::write::DeflateEncoder;
 use flate2::Compression;
 
-use serde::{Deserialize, Serialize};
+pub use map::*;
 
 static MAGIC: &[u8] = b"\xffBARS\x13eu";
-const VERSION: u16 = 0;
+const VERSION: u16 = 1;
 
-fn bincode_options() -> impl Options {
-	DefaultOptions::new().with_limit(0x100_0000)
-}
+const BINCODE_CONFIG: BincodeConfig = bincode::config::standard();
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Decode, Encode)]
 pub struct Config {
 	pub name: Option<String>,
 	pub version: Option<String>,
@@ -26,35 +30,53 @@ pub struct Config {
 }
 
 impl Config {
-	pub fn load(mut reader: impl Read) -> bincode::Result<Self> {
+	pub fn load(mut reader: impl Read) -> Result<Self, DecodeError> {
+		fn bincode_error(error: IoError) -> DecodeError {
+			DecodeError::Io {
+				inner: error,
+				additional: 0,
+			}
+		}
+
 		let mut buf = vec![0; MAGIC.len()];
-		reader.read_exact(&mut buf)?;
+		reader.read_exact(&mut buf).map_err(bincode_error)?;
 
 		if buf != MAGIC {
-			return Err(ErrorKind::Custom("invalid config file".into()).into())
+			return Err(DecodeError::Other("invalid config file"))
 		}
 
 		let mut buf = [0; 2];
-		reader.read_exact(&mut buf)?;
+		reader.read_exact(&mut buf).map_err(bincode_error)?;
 
 		if buf != VERSION.to_be_bytes() {
-			return Err(ErrorKind::Custom("unsupported config version".into()).into())
+			return Err(DecodeError::Other("unsupported config version"))
 		}
 
-		let reader = DeflateDecoder::new(reader);
-		bincode_options().deserialize_from(reader)
+		let mut reader = DeflateDecoder::new(reader);
+		bincode::decode_from_std_read(&mut reader, BINCODE_CONFIG)
 	}
 
-	pub fn save(&self, mut writer: impl Write) -> bincode::Result<()> {
-		writer.write_all(&MAGIC)?;
-		writer.write_all(&VERSION.to_be_bytes())?;
+	pub fn save(&self, mut writer: impl Write) -> Result<(), EncodeError> {
+		fn bincode_error(error: IoError) -> EncodeError {
+			EncodeError::Io {
+				inner: error,
+				index: 0,
+			}
+		}
 
-		let writer = DeflateEncoder::new(writer, Compression::best());
-		bincode_options().serialize_into(writer, self)
+		writer.write_all(&MAGIC).map_err(bincode_error)?;
+		writer
+			.write_all(&VERSION.to_be_bytes())
+			.map_err(bincode_error)?;
+
+		let mut writer = DeflateEncoder::new(writer, Compression::best());
+		bincode::encode_into_std_write(self, &mut writer, BINCODE_CONFIG)?;
+
+		Ok(())
 	}
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Decode, Encode)]
 pub struct Aerodrome {
 	pub icao: String,
 
@@ -65,64 +87,118 @@ pub struct Aerodrome {
 
 	pub profiles: Vec<Profile>,
 
+	pub geo_map: Option<GeoMap>,
 	pub maps: Vec<Map>,
-	pub views: Vec<View>,
 	pub styles: Vec<Style>,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+impl Aerodrome {
+	pub fn decode(serialised: &[u8]) -> Result<Self, DecodeError> {
+		Ok(bincode::decode_from_slice(serialised, BINCODE_CONFIG)?.0)
+	}
+
+	pub fn encode(&self) -> Result<Vec<u8>, EncodeError> {
+		bincode::encode_to_vec(self, BINCODE_CONFIG)
+	}
+}
+
+#[derive(Debug, Decode, Encode)]
+pub struct Ref<T>(pub usize, PhantomData<T>);
+
+impl<T> Clone for Ref<T> {
+	fn clone(&self) -> Self {
+		*self
+	}
+}
+
+impl<T> Copy for Ref<T> {}
+
+impl<T> Hash for Ref<T> {
+	fn hash<H: Hasher>(&self, state: &mut H) {
+		self.0.hash(state);
+	}
+}
+
+impl<T> PartialEq for Ref<T> {
+	fn eq(&self, other: &Self) -> bool {
+		self.0 == other.0
+	}
+}
+
+impl<T> Eq for Ref<T> {}
+
+impl<T> PartialOrd for Ref<T> {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		self.0.partial_cmp(&other.0)
+	}
+}
+
+impl<T> Ord for Ref<T> {
+	fn cmp(&self, other: &Self) -> Ordering {
+		self.0.cmp(&other.0)
+	}
+}
+
+impl<T> From<usize> for Ref<T> {
+	fn from(from: usize) -> Self {
+		Self(from, PhantomData)
+	}
+}
+
+impl<T> From<Ref<T>> for usize {
+	fn from(from: Ref<T>) -> Self {
+		from.0
+	}
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
 pub struct Element {
 	pub id: String,
 	pub condition: ElementCondition,
 }
 
 #[derive(
-	Clone,
-	Copy,
-	Debug,
-	Hash,
-	PartialEq,
-	Eq,
-	PartialOrd,
-	Ord,
-	Deserialize,
-	Serialize,
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
 )]
 pub enum ElementCondition {
 	Fixed(bool),
-	Node(usize),
-	Edge(usize),
+	Node(Ref<Node>),
+	Edge(Ref<Edge>),
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Decode, Encode)]
 pub struct Node {
 	pub id: String,
 
 	pub scratchpad: Option<String>,
-	pub parent: Option<usize>,
-
-	pub display: NodeDisplay<GeoPoint>,
+	pub parent: Option<Ref<Node>>,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Edge {
-	pub display: EdgeDisplay<GeoPoint>,
-}
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct Edge;
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Decode, Encode)]
 pub struct Block {
 	pub id: String,
 
-	pub nodes: Vec<usize>,
-	pub edges: Vec<usize>,
-	pub non_routes: Vec<(usize, usize)>,
+	/// parent nodes only
+	pub nodes: Vec<Ref<Node>>,
+	pub edges: Vec<Ref<Edge>>,
+	pub non_routes: Vec<BlockRoute>,
 
 	pub stands: Vec<String>,
-
-	pub display: BlockDisplay<GeoPoint>,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+/// child nodes only
+pub struct BlockRoute {
+	pub from: Ref<Node>,
+	pub to: Ref<Node>,
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
 pub struct Profile {
 	pub id: String,
 	pub name: String,
@@ -135,225 +211,116 @@ pub struct Profile {
 }
 
 #[derive(
-	Clone,
-	Copy,
-	Debug,
-	Hash,
-	PartialEq,
-	Eq,
-	PartialOrd,
-	Ord,
-	Deserialize,
-	Serialize,
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
 )]
 pub enum NodeCondition {
-	Fixed { state: bool },
+	Fixed { state: NodeState },
 	Direct { reset: ResetCondition },
-	Router,
+	Router { sticky: bool },
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Decode, Encode)]
 pub enum EdgeCondition {
 	Fixed {
-		state: bool,
+		state: EdgeState,
 	},
 	Direct {
-		node: usize,
+		nodes: NodeExpression,
 	},
 	Router {
-		block: usize,
-		routes: Vec<(usize, usize)>,
+		block: Ref<Block>,
+		routes: Vec<BlockRoute>,
 	},
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct NodeExpression {
+	pub disjunction: Vec<NodeConjunction>,
+}
+
+impl NodeExpression {
+	pub fn evaluate(
+		&self,
+		node_state: &impl Fn(Ref<Node>) -> NodeState,
+	) -> EdgeState {
+		if self
+			.disjunction
+			.iter()
+			.any(|conjunction| conjunction.evaluate(node_state))
+		{
+			EdgeState::On
+		} else {
+			EdgeState::Off
+		}
+	}
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct NodeConjunction {
+	pub positive: Vec<Ref<Node>>,
+	pub negative: Vec<Ref<Node>>,
+}
+
+impl NodeConjunction {
+	fn evaluate(&self, node_state: &impl Fn(Ref<Node>) -> NodeState) -> bool {
+		self
+			.positive
+			.iter()
+			.all(|node| node_state(*node) == NodeState::On)
+			&& self
+				.negative
+				.iter()
+				.all(|node| node_state(*node) == NodeState::Off)
+	}
 }
 
 #[derive(
-	Clone,
-	Copy,
-	Debug,
-	Hash,
-	PartialEq,
-	Eq,
-	PartialOrd,
-	Ord,
-	Deserialize,
-	Serialize,
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
 )]
 pub struct BlockCondition {
 	pub reset: ResetCondition,
 }
 
 #[derive(
-	Clone,
-	Copy,
-	Debug,
-	Hash,
-	PartialEq,
-	Eq,
-	PartialOrd,
-	Ord,
-	Deserialize,
-	Serialize,
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
 )]
 pub enum ResetCondition {
 	None,
 	TimeSecs(u32),
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Decode, Encode)]
 pub struct Preset {
 	pub name: String,
 
-	pub nodes: Vec<(usize, NodeState)>,
-	pub blocks: Vec<(usize, BlockState)>,
+	pub nodes: Vec<(Ref<Node>, NodeState)>,
+	pub blocks: Vec<(Ref<Block>, BlockState)>,
 }
 
-type NodeState = bool;
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+#[repr(u8)]
+pub enum NodeState {
+	Off,
+	On,
+}
 
 #[derive(
-	Clone,
-	Copy,
-	Debug,
-	Hash,
-	PartialEq,
-	Eq,
-	PartialOrd,
-	Ord,
-	Deserialize,
-	Serialize,
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+#[repr(u8)]
+pub enum EdgeState {
+	Off,
+	On,
+}
+
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
 )]
 pub enum BlockState {
 	Clear,
 	Relax,
-	Route((usize, usize)),
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Map {
-	pub background: Color,
-	pub base: Vec<Path<Point>>,
-
-	pub nodes: Vec<NodeDisplay<Point>>,
-	pub edges: Vec<EdgeDisplay<Point>>,
-	pub blocks: Vec<BlockDisplay<Point>>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct View {
-	pub name: String,
-
-	pub map: usize,
-	pub bounds: Box,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Deserialize, Serialize)]
-pub struct Box {
-	pub min: Point,
-	pub max: Point,
-}
-
-#[derive(
-	Clone, Copy, Debug, Default, PartialEq, PartialOrd, Deserialize, Serialize,
-)]
-pub struct Point {
-	pub x: f32,
-	pub y: f32,
-}
-
-#[derive(
-	Clone, Copy, Debug, Default, PartialEq, PartialOrd, Deserialize, Serialize,
-)]
-pub struct Geo {
-	pub lat: f32,
-	pub lon: f32,
-}
-
-#[derive(
-	Clone, Copy, Debug, Default, PartialEq, PartialOrd, Deserialize, Serialize,
-)]
-pub struct GeoPoint {
-	pub geo: Geo,
-	pub offset: Point,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Path<T: Clone + Debug> {
-	pub points: Vec<T>,
-	pub style: usize,
-}
-
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
-pub struct Target<T: Clone + Debug> {
-	pub points: Vec<T>,
-}
-
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
-pub struct NodeDisplay<T: Clone + Debug> {
-	pub off: Vec<Path<T>>,
-	pub on: Vec<Path<T>>,
-	pub selected: Vec<Path<T>>,
-
-	pub target: Target<T>,
-}
-
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
-pub struct EdgeDisplay<T: Clone + Debug> {
-	pub off: Vec<Path<T>>,
-	pub on: Vec<Path<T>>,
-}
-
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
-pub struct BlockDisplay<T: Clone + Debug> {
-	pub target: Target<T>,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Style {
-	pub stroke_width: f32,
-	pub stroke_color: Color,
-
-	pub fill_style: FillStyle,
-	pub fill_color: Color,
-}
-
-#[derive(
-	Clone,
-	Copy,
-	Debug,
-	Default,
-	Hash,
-	PartialEq,
-	Eq,
-	PartialOrd,
-	Ord,
-	Deserialize,
-	Serialize,
-)]
-pub struct Color {
-	pub r: u8,
-	pub g: u8,
-	pub b: u8,
-	pub a: u8,
-}
-
-#[derive(
-	Clone,
-	Copy,
-	Debug,
-	Hash,
-	PartialEq,
-	Eq,
-	PartialOrd,
-	Ord,
-	Deserialize,
-	Serialize,
-)]
-pub enum FillStyle {
-	None,
-	Solid,
-	HatchHorizontal,
-	HatchVertical,
-	HatchForwardDiagonal,
-	HatchBackwardDiagonal,
-	HatchCross,
-	HatchDiagonalCross,
+	/// parent nodes
+	Route((Ref<Node>, Ref<Node>)),
 }
---
+++ shared/config/src/map.rs
@@ -0,0 +1,175 @@
+use super::*;
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct Style {
+	pub stroke_style: StrokeStyle,
+	pub stroke_width: f32,
+	pub stroke_cap: StrokeCap,
+	pub stroke_join: StrokeJoin,
+	pub stroke_color: Color,
+
+	pub fill_style: FillStyle,
+	pub fill_color: Color,
+}
+
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+pub struct Color {
+	pub r: u8,
+	pub g: u8,
+	pub b: u8,
+	pub a: u8,
+}
+
+impl Default for Color {
+	fn default() -> Self {
+		Self {
+			r: 0xff,
+			g: 0x00,
+			b: 0xff,
+			a: 0x00,
+		}
+	}
+}
+
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+pub enum StrokeStyle {
+	None,
+	Dash(i32),
+}
+
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+pub struct StrokeCap(pub i32);
+
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+pub struct StrokeJoin(pub i32);
+
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+pub enum FillStyle {
+	None,
+	Fill,
+	Hatch(i32),
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct GeoMap {
+	pub nodes: Vec<NodeDisplay<GeoPoint>>,
+	pub edges: Vec<EdgeDisplay<GeoPoint>>,
+	pub blocks: Vec<BlockDisplay<GeoPoint>>,
+	pub widgets: Vec<Widget<GeoPoint>>,
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct Map {
+	pub background: Color,
+	pub base: Vec<Path<Point>>,
+
+	pub nodes: Vec<NodeDisplay<Point>>,
+	pub edges: Vec<EdgeDisplay<Point>>,
+	pub blocks: Vec<BlockDisplay<Point>>,
+	pub widgets: Vec<Widget<Point>>,
+
+	pub views: Vec<View>,
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct View {
+	pub name: String,
+	pub bounds: Box,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Decode, Encode)]
+pub struct Box {
+	pub min: Point,
+	pub max: Point,
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub struct Path<T: Projectable> {
+	pub points: Vec<T>,
+	pub style: Ref<Style>,
+}
+
+#[derive(Clone, Debug, Default, Decode, Encode)]
+pub struct Target<T: Projectable> {
+	pub polygons: Vec<Vec<T>>,
+}
+
+#[derive(Clone, Debug, Default, Decode, Encode)]
+pub struct NodeDisplay<T: Projectable> {
+	pub off: Vec<Path<T>>,
+	pub on: Vec<Path<T>>,
+	pub selected: Vec<Path<T>>,
+
+	pub target: Target<T>,
+}
+
+#[derive(Clone, Debug, Default, Decode, Encode)]
+pub struct EdgeDisplay<T: Projectable> {
+	pub off: Vec<Path<T>>,
+	pub on: Vec<Path<T>>,
+	pub pending: Vec<Path<T>>,
+}
+
+#[derive(Clone, Debug, Default, Decode, Encode)]
+pub struct BlockDisplay<T: Projectable> {
+	pub target: Target<T>,
+}
+
+#[derive(Clone, Debug, Decode, Encode)]
+pub enum Widget<T: Projectable> {
+	Countdown {
+		position: T,
+		size: f32,
+		condition: CountdownCondition,
+	},
+}
+
+#[derive(
+	Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Decode, Encode,
+)]
+pub enum CountdownCondition {
+	Node(Ref<Node>),
+	Block(Ref<Block>),
+}
+
+pub trait Projectable: Clone + Debug {}
+
+#[derive(
+	Clone, Copy, Debug, Default, PartialEq, PartialOrd, Decode, Encode,
+)]
+pub struct Point {
+	pub x: f32,
+	pub y: f32,
+}
+
+impl Projectable for Point {}
+
+#[derive(
+	Clone, Copy, Debug, Default, PartialEq, PartialOrd, Decode, Encode,
+)]
+pub struct Geo {
+	pub lat: f32,
+	pub lon: f32,
+}
+
+impl Projectable for Geo {}
+
+#[derive(
+	Clone, Copy, Debug, Default, PartialEq, PartialOrd, Decode, Encode,
+)]
+pub struct GeoPoint {
+	pub geo: Geo,
+	pub offset: Point,
+}
+
+impl Projectable for GeoPoint {}
